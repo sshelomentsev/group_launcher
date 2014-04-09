bb00714fb5942621516b964c73e9847aa63ab992
Sergey Shelomentsev <shelomencevsergey@gmail.com>
2014-04-09 17:42:52 +0700

    implemented inherited launch

--- src/sshelomentsev/grouplauncher/GroupLauncherDelegate.java
+++ src/sshelomentsev/grouplauncher/GroupLauncherDelegate.java
@@ -1,5 +1,7 @@
 package sshelomentsev.grouplauncher;
 
+import java.util.LinkedList;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
@@ -7,12 +9,11 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 
 public class GroupLauncherDelegate extends LaunchConfigurationDelegate{
-
-	@Override
-	public void launch(ILaunchConfiguration configuration, String mode,
-			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		// TODO Auto-generated method stub
-		
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		LinkedList<ILaunch> launches = new LinkedList<ILaunch>();
+		for (ILaunchConfiguration child : GroupLauncher.getDefault().getConfigurations(configuration)) {
+			launches.add(child.launch(mode, monitor));
+		}
+		launch.addProcess(new GroupProcess(launch, launches));
 	}
-
 }
